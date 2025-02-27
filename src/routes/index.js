203d13ca0991e5a5619a797471bb498e3891e458
eeuhhnil <k60duongthihuelinh@gmail.com>
2025-02-27 22:55:49 +0300

    space

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,17 +4,14 @@ import Register from "~/pages/register";
 import BoardingPage from "~/pages/boarding";
 import CreateSpace from "~/pages/createSpace";
 import LandingLayout from "~/layout/components/Landing/LandingLayout";
-import Home from "~/pages/home";
-import Calendar from "~/pages/calender";
 import Project from "~/pages/project";
 import ProjectLayout from "~/layout/components/ProjectLayout";
-import ProjectDetail from "~/pages/projectDetail";
-import Members from "~/pages/members";
-import Setting from "~/pages/settings";
 import HeaderOnly from "~/layout/HeaderOnly";
 import UserProfile from "~/pages/UserProfile";
 import HomeLayout from "~/layout/HomeLayout";
 import DropdownItem from "~/pages/fggf";
+import Home from "~/pages/home";
+
 
 export const publicRoutes = [
   { path: "/", component: LandingPage, layout: LandingLayout },
@@ -22,14 +19,10 @@ export const publicRoutes = [
   { path: "/register", component: Register, layout: null },
   { path: "/boarding", component: BoardingPage, layout: null },
   { path: "/boarding/new", component: CreateSpace, layout: null },
-  { path: "/home", component: Home, layout: HomeLayout, layoutType: "home" },
+  { path: "/space/:spaceId",component: Home, layout: HomeLayout, layoutType: "home"},
   { path: "/project", component: Project, layout: HeaderOnly },
   { path: "/project/:projectId", component: ProjectLayout, layout: null },
   { path: "/aa", component: DropdownItem, layout: null },
-
   { path: "/profile", component: UserProfile, layout: HeaderOnly },
-  // { path: "/projectDetail", component: ProjectLayout, layout: null },
-  // { path: "/calendar", component: Calendar, layout: null },
-  // { path: "/members", component: Members, layout: ProjectLayout },
-  // { path: "/setting", component: Setting, layout: ProjectLayout },
+
 ];
